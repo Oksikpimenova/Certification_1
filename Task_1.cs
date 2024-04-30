a428472de2b2c9fda3e4cd822d4cc1ed5ec0e416
Mikhail Pimenov <tomorrowind@ya.ru>
2024-04-30 09:19:51 +0300

    intermidiate commit 2

--- Task_1.cs
+++ Task_1.cs
@@ -1,8 +1,8 @@
 //Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
 string Len(int stop, int start)
 {
-if (start == stop) return Convert.ToString(stop);
-return start + " " + Len(stop, start + 1);
+  if (start == stop) return Convert.ToString(stop);
+    return start + " " + Len(stop, start + 1);
 }
 
 Console.WriteLine("Введите числа: m n ");
